551ed8093f86bf414f98b20cb87bfe467062aa59
davila <davilaketilly59@gmail.com>
2025-05-08 11:14:08 -0300

    ativ 1e2

--- biblioteca.py
+++ biblioteca.py
@@ -6,11 +6,18 @@ class Pessoa():
         self.comendo=False
         self.falando=False
         self.dormindo=False
-    def falar(self):
-        print (f" {self.nome} comecou a falar ")
-    def comer(self,comida):
-        if self.comendo==true:
-            print("nao pode comer, pois ja esta comendo")
-     print (f" comecou a comer {comida}")
-    def dormir (self):
-        print (f" comecou a dormir ")
+    def dormir(self):
+       if self.dormindo == True:
+           print(f" {self.nome} ja estar dormindo")
+       elif self.falando==True:
+           print (f" {self.nome} nao pode falar pq ta dormindo")
+       elif self.comendo==True:
+           print (f" {self.nome} nao pode comer pq ta falando")
+       else:
+           print (f" foi dormir")
+       elif self.dormindo==True:
+      def self.
+       elif self.comendo=False
+   else:
+          print  (f" ele ja estar acordado")
+
--- exercicio01.py
+++ exercicio01.py
@@ -6,3 +6,4 @@ aluno01 = Pessoa("guilherme",18, 75)
 aluno01.dormir()
 aluno01.falar()
 aluno01.comer("lasanha")
+
